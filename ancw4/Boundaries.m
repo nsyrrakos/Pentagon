3cfb243e159fcd181923710d2cf4fce3db7189f3
nsyrrakos <syrrakos.nik@gmail.com>
2020-12-07 19:24:15 +0200

    Create Boundaries.m

---
+++ ancw4/Boundaries.m
@@ -0,0 +1,41 @@
+Boundaries = {gb[1] -> (E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+       ((-S51)^ep*Gamma[1 - 2*ep]), 
+     gb[2] -> (E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+       ((S12 - S34)^ep*Gamma[1 - 2*ep]), 
+     gb[3] -> (E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+       ((-S12)^ep*Gamma[1 - 2*ep]), 
+     gb[4] -> -((E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+         ((-1 + 2*ep)*(-S45)^ep*Gamma[1 - 2*ep])) + 
+       (2*E^(ep*EulerGamma)*ep*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(-S45)^ep*Gamma[1 - 2*ep]), 
+     gb[5] -> (E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+       ((-S45)^ep*Gamma[1 - 2*ep]), 
+     gb[6] -> (E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+       ((-S45)^ep*Gamma[1 - 2*ep]), 
+     gb[7] -> (E^(ep*EulerGamma)*ep*(-S12)^(-1 - ep)*S12*Gamma[-ep]^2*
+        Gamma[1 + ep]*Hypergeometric2F1[1, -ep, 1 - ep, 
+         -((-S12 + S34 - S51)/S51)])/Gamma[-2*ep], 
+     gb[8] -> (-2*E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(-S51)^ep*Gamma[1 - 2*ep]) + 
+       (4*E^(ep*EulerGamma)*ep*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(-S51)^ep*Gamma[1 - 2*ep]), 
+     gb[9] -> (E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(-S12)^ep*Gamma[1 - 2*ep]) - 
+       (2*E^(ep*EulerGamma)*ep*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(-S12)^ep*Gamma[1 - 2*ep]) - 
+       (2*E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(S12 - S34)^ep*Gamma[1 - 2*ep]) + 
+       (4*E^(ep*EulerGamma)*ep*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(S12 - S34)^ep*Gamma[1 - 2*ep]), 
+     gb[10] -> (2*E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(S12 - S34)^ep*Gamma[1 - 2*ep]) - 
+       (4*E^(ep*EulerGamma)*ep*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(S12 - S34)^ep*Gamma[1 - 2*ep]) - 
+       (2*E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(-S51)^ep*Gamma[1 - 2*ep]) + 
+       (4*E^(ep*EulerGamma)*ep*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(-S51)^ep*Gamma[1 - 2*ep]), gb[11] -> 0, 
+     gb[12] -> -((E^(ep*EulerGamma)*Gamma[1 - ep]^2*Gamma[1 + ep])/
+         ((-1 + 2*ep)*(-S12)^ep*Gamma[1 - 2*ep])) + 
+       (2*E^(ep*EulerGamma)*ep*Gamma[1 - ep]^2*Gamma[1 + ep])/
+        ((-1 + 2*ep)*(-S12)^ep*Gamma[1 - 2*ep]), gb[13] -> 0}
